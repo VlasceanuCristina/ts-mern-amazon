b6ffc4b71fa7b131f8d4c2e2563c32df638cc1fc
cristina_vlasceanu <vlasceanucristina7@gmail.com>
2024-02-16 18:34:55 +0200

    added react-router-dom

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
     3. add product images
     4. render products
 
+4. Add bootstap
+    npm install react-bootstrap bootstrap
+
+5. Page routing
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+  //Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/*<Route path="contact" element={<Contact />} />*/}
+      
+    </Route>
+  )
+);
+
+
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router ={router}></RouterProvider>
   </React.StrictMode>,
 )
