14323abc24f002d664fe4af35ab663dcc5db13eb
cristina_vlasceanu <vlasceanucristina7@gmail.com>
2024-02-16 18:19:43 +0200

    used boostrap css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,40 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap'
 import { sampleProducts } from './data'
 
 function App() {
- // const [count, setCount] = useState(0)
-
+ 
   return (
-    <div>
+    <div className="d-flex flex-column vh-100">
       <header>
-        TS Amazona
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>
+              TS Amazona
+            </Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className='nav-link'>Cart</a>
+            <a href="/signin" className='nav-link'>Sign</a>
+          </Nav>
+        </Navbar>
       </header>
       <main>
-        <ul>
-        {
-        
-        sampleProducts.map( (product) => (
-        <li key ={product.slug}>
-          <img src={product.image} alt={product.name} className ="product-image"></img>
-          <h2>{product.name}</h2>
-          <h2>${product.price}</h2>
-        </li>
-        ))}
-        </ul>
-
+        <Container className="mt-3">
+          <Row>
+            {            
+            sampleProducts.map( (product) => (
+            <Col key ={product.slug} sm={6} md={4} lg={3}>
+              <img src={product.image} alt={product.name} className ="product-image"></img>
+              <h2>{product.name}</h2>
+              <h2>${product.price}</h2>
+            </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
       <footer>
-        All right reserved
+        <div className="text-center">All rights reserved</div>
       </footer>
     </div>
   )
